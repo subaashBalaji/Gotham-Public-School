9ac1c1459610d63c307f2194a9800413b44288e0
Subaash Balaji <78993557+subaashBalaji@users.noreply.github.com>
2022-03-17 13:51:42 +0530

    Basic Initial Version
    this code is just the initialization for the project.

---
+++ main.sql
@@ -0,0 +1,13 @@
+-- create a table
+CREATE TABLE student (
+  student_id INTEGER PRIMARY KEY,
+  student_name TEXT,
+  class INTEGER,
+  dob DATE
+);
+CREATE TABLE teacher (
+  teacher_id INTEGER PRIMARY KEY,
+  teacher_name TEXT,
+  class INTEGER,
+  salary INTEGER
+);
